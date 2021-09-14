0474d7f0cc56e261903640fafd084d243a25c604
brandon-sharpe <brandon.robert.sharpe@gmail.com>
2021-09-14 15:47:00 -0500

    final draft semi complete need to update README.md

--- explore.py
+++ explore.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import wrangle
+import sklearn.preprocessing
 import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.preprocessing import PolynomialFeatures
+
+import warnings
+warnings.filterwarnings("ignore")
 
 def tax_distro(df):
     '''This will graph the tax distubution between counties '''
@@ -13,3 +22,85 @@ def tax_distro(df):
     plt.xlabel('Tax Rate Percentage', fontsize = 15)
     plt.ylabel('Frequency', fontsize = 15)
     plt.title('Distributions of Tax Rates by County', x = .24, fontsize = 20)
+
+def distrubutions(train):
+    '''checking distributions of our selected features/univariate exploration'''
+    for x in train[['bedrooms','bathrooms','sqft','tax_value','has_pool','county', 'year_built']]:
+        sns.histplot(train[x], bins = 20, kde = True)
+        plt.title(f'Distribution of {x}')
+        plt.tight_layout()
+        plt.show()
+
+def corr_heatmap(train): 
+# Make a heatmap that shows correlation of churn and other variables
+    plt.figure(figsize=(8, 12))
+    heatmap = sns.heatmap(train.drop(columns=[
+                                              'tax_rate']).corr()[['tax_value']].sort_values(by='tax_value',
+                                                                                                          ascending=False), vmin=-1, vmax=1, annot=True, cmap='BrBG')
+    heatmap.set_title('Features Correlating with Tax Value', fontdict={'fontsize':18}, pad=16);
+
+
+        
+def add_scaled_columns(train, validate, test, scaler, columns_to_scale):
+    '''Takes in df and scales the columns inputed and concats them to the dataframe '''    
+    # new column names
+    new_column_names = [c + '_scaled' for c in columns_to_scale]
+    
+    # Fit the scaler on the train
+    scaler.fit(train[columns_to_scale])
+    
+    # transform train validate and test
+    train = pd.concat([
+        train,
+        pd.DataFrame(scaler.transform(train[columns_to_scale]), columns=new_column_names, index=train.index),
+    ], axis=1)
+    
+    validate = pd.concat([
+        validate,
+        pd.DataFrame(scaler.transform(validate[columns_to_scale]), columns=new_column_names, index=validate.index),
+    ], axis=1)
+    
+    
+    test = pd.concat([
+        test,
+        pd.DataFrame(scaler.transform(test[columns_to_scale]), columns=new_column_names, index=test.index),
+    ], axis=1)
+    
+    return train, validate, test
+
+def lets_explore(train):
+    distrubutions(train)
+    corr_heatmap(train)
+    
+def scat_plot(df,x,y):
+    plt.figure(figsize = (12,10))
+    sns.lmplot(x = (x), y=(y), data = df,line_kws={'color': 'red'})
+    plt.title(f'Relationship between {x} and {y}', loc = 'left')
+    plt.tight_layout()
+    plt.ticklabel_format(style='plain')
+    plt.show()
+    
+def make_metric_df(y, y_pred, model_name, metric_df):
+    if metric_df.size ==0:
+        metric_df = pd.DataFrame(data=[
+            {
+                'model': model_name, 
+                'RMSE_validate': round(mean_squared_error(
+                    y,
+                    y_pred) ** .5),
+                'r^2_validate': explained_variance_score(
+                    y,
+                    y_pred)
+            }])
+        return metric_df
+    else:
+        return metric_df.append(
+            {
+                'model': model_name, 
+                'RMSE_validate': mean_squared_error(
+                    y,
+                    y_pred) ** .5,
+                'r^2_validate': explained_variance_score(
+                    y,
+                    y_pred)
+            }, ignore_index=True)
--- wrangle.py
+++ wrangle.py
@@ -159,11 +159,11 @@ def prepare_zillow():
     df[slc] = df[slc].astype(int)
     # converting column data thats categorical into objects so that tgey wont be used in mathmetics
     df.fips = df.fips.astype(object)
-    df.year_built = df.year_built.astype(object)
     df.has_pool=df.has_pool.astype(int)
     counties = {6037: 'LA', 6059: 'Orange', 6111: 'Ventura'}
     df['county'] = df.fips.replace(counties)
     df['tax_rate'] = round((df.tax_amount / df.tax_value) *100,2)
+    
     # train/validate/test split
     train_validate, test = train_test_split(df, test_size=.2, random_state=123)
     train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
